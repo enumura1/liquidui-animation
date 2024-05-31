f1a290c10ad7650b3b16d32c10d3a0824e1745fd
enumura1 <akirayuto0718@gmail.com>
2024-05-31 14:53:03 +0900

    update:lint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true , "node": true},
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "quotes": ["error", "double", { "avoidEscape": true, "allowTemplateLiterals": true }],
   },
 }
--- src/utils/sizeHandler.tsx
+++ src/utils/sizeHandler.tsx
@@ -1,38 +1,34 @@
-import { getSizeFromOption } from './selectSize';
+import { getSizeFromOption } from "./selectSize";
 
 type Size = {
   width: string;
   height: string;
 };
 
-type SizeOption = 'small' | 'middle' | 'large';
+type SizeOption = "small" | "middle" | "large";
 
 type SetSizesProps = {
   size?: Size | SizeOption;
 };
 
-const DEFAULT_SIZE: Size = { width: '200px', height: '200px' };
+const DEFAULT_SIZE: Size = { width: "200px", height: "200px" };
 
-// UIサイズを計算する関数
 const calculateUiSize = (size?: Size | SizeOption): Size => {
-  // sizeがオブジェクトの場合はそのまま返す
-  if (typeof size === 'object') {
+  
+  if (typeof size === "object") {
     return size;
   } 
-  // sizeが文字列の場合は、対応するオプションを取得して返す
-  else if (typeof size === 'string') {
+  else if (typeof size === "string") {
     return getSizeFromOption(size);
   } 
-  // sizeが未指定の場合はデフォルトサイズを返す
   else {
-    console.error('Size not specified. Using default size.');
+    console.error("Size not specified. Using default size.");
     return DEFAULT_SIZE;
   }
 };
 
-// SizeHandlerコンポーネント
+
 const SizeHandler = ({ size }: SetSizesProps) => {
-  // UIサイズを計算する
   const uiSize = calculateUiSize(size);
   
   return uiSize;
